75476ca750cf86de3cfb39efa951f398ad1b48b2
giacomo-carugati <giacomo.carugati@mail.polimi.it>
2024-06-03 23:35:17 +0200

    20240206_Pb2: a), b)

---
+++ 2024/20240206/Pb2.R
@@ -0,0 +1,88 @@
+###----------------------------------------------------------###
+### Problem 2: Quality control in food production (20240206) ###
+###----------------------------------------------------------###
+
+
+rm(list = ls())
+graphics.off()
+
+options(rgl.printRglwidget = TRUE)
+
+library(MVN)
+library(MASS)
+
+data <- read.table('food.txt', header = T)
+head(data)
+
+hl <- factor(data$result)
+col.lab <- ifelse(hl %in% c("high"), 'red', 'blue')
+
+plot(data[,1:4], col = col.lab, pch=20)
+
+high <- data[which(data$result == "high"), 1:4]
+low <- data[which(data$result == "low"), 1:4]
+
+
+# a) ----------------------------------------------------------------------
+
+mvn(high)$multivariateNormality
+mvn(low)$multivariateNormality
+
+# Normality is not respected for the "low" group
+
+var.test(high[, 1], low[, 1])
+var.test(high[, 2], low[, 2])
+var.test(high[, 3], low[, 3])
+var.test(high[, 4], low[, 4])
+
+# On the diagonal, we have no problems
+
+hCov <- cov(high)
+lCov <- cov(low)
+
+list(S1 = hCov, S2 = lCov)
+
+# Using the rule of thumb, the homogeneity assumption seems to hold
+
+# -> LDA
+
+
+# b) ----------------------------------------------------------------------
+
+c.hl <- 100000
+c.lh <- 500
+
+pl <- 0.001
+ph <- 1-0.001
+prior = c(ph, pl)
+prior
+
+prior.c <- c(ph*c.lh/(c.hl*pl+c.lh*ph), pl*c.hl/(c.hl*pl+c.lh*ph))
+prior.c
+
+lda <- lda(data[, 1:4], hl, prior = prior.c)
+lda
+
+lda.pred <- predict(lda, data[, 1:4])
+
+G <- 2
+misc <- table(class.true = hl, class.assigned = lda.pred$class)
+APER <- 0
+for(g in 1:G)
+  APER <- APER + sum(misc[g,-g])/sum(misc[g,]) * prior.c[g]
+
+print(paste("APER:", APER))
+
+errors_CV <- 0
+for(i in 1:60)
+{
+  ldaCV.i <- lda(data[-i, 1:4], hl[-i], prior = prior.c)
+  errors_CV <- errors_CV + as.numeric(predict(ldaCV.i, data[i, 1:4])$class != hl[i])
+}
+
+AERCV <- sum(errors_CV)/length(hl)
+print(paste("AERCV:", AERCV))
+
+# APER is 0.1445, while AERCV is 0.3
+# We don't have enough observations, therefore the estimates of our parameters are a bit rough
+# and our classifier performs badly on unseen data
