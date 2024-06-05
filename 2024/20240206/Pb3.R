10d3613730b4baa8328ba485ff700bc83759e247
giacomo-carugati <giacomo.carugati@mail.polimi.it>
2024-06-05 16:30:48 +0200

    20240206_Pb3: b), c), d)

--- 2024/20240206/Pb3.R
+++ 2024/20240206/Pb3.R
@@ -9,6 +9,10 @@ graphics.off()
 library(MASS)
 library(car)
 library(rgl)
+library(nlme)
+library(lme4)
+library(insight)
+library(lattice)
 
 options(rgl.printRglwidget = TRUE)
 
@@ -33,6 +37,54 @@ shapiro.test(residuals(m0))
 
 # Normality verified
 
+# b) ----------------------------------------------------------------------
+
+alpha = 0.1
+print("90% confidence interval for age coefficient"); confint(m0, level = 1-alpha)[3, ]
+
+# No, at 90% confidence level we can't affirm age has a positive effect on asthma prevalence
+# (we would at 80%)
+
+alpha = 0.05
+print("95% confidence interval for urbanYes coefficient"); confint(m0, level = 1-alpha)[2, ]
+
+
+# c) ----------------------------------------------------------------------
+
+m0_red <- lm(asthma ~ urban + age + pollution + income + education, data = data)
+summary(m0_red)
+
+m0_red <- lm(asthma ~ age + pollution + income + education, data = data)
+summary(m0_red)
+
+m0_red <- lm(asthma ~ pollution + income + education, data = data)
+summary(m0_red)
+
+m0_red.form <- formula(asthma ~ pollution + income + education)
+m1 <- gls(m0_red.form, correlation = corCompSymm(form = ~1|region_id), data = data)
+summary(m1)
+
+alpha = 0.01
+intervals(m1, which = "var-cov", level = 1-alpha)
+
+
+# d) ----------------------------------------------------------------------
+
+m2 <- lmer(asthma ~ pollution + income + education + (1|region_id), data = data)
+summary(m2)
+
+sigma2_eps <- as.numeric(get_variance_residual(m2))
+sigma2_b <- as.numeric(get_variance_random(m2))
+
+PVRE <- sigma2_b/(sigma2_b+sigma2_eps)
+PVRE
+
+# The percentage of the region variance out of the total is really high (70%)
+
+dotplot(ranef(m2, condVar=T))
+confint(m2)
+
+VarCorr(m2)
 
 # Extra -------------------------------------------------------------------
 
