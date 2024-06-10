9c2b87f23c543c0efd88e234e9ce3c0b8c107a53
Filippobuda <100417062+Filippobuda@users.noreply.github.com>
2024-06-10 19:15:00 +0200

    Giacomino correction

--- 2024/20240206/Pb2.R
+++ 2024/20240206/Pb2.R
@@ -5,6 +5,16 @@ library(MVN)
 data <- read.table("food.txt",header=TRUE)
 features <- data[,1:4]
 
+result <- factor(data$result)
+
+low <- features[which(result=="low"),]
+high <- features[which(result=="high"),]
+
+
+plot(features, pch=19)
+plot(low, col='red', pch=19)
+plot(high, col='blue', pch=19)
+
 # cost of misclassifications c_ij of attributing to j a unit belonging to i
 cost.highlow <- 100000
 cost.lowhigh <- 500
@@ -16,6 +26,8 @@ p.high <-1 - p.low
 p.low.new <- p.low * cost.highlow / (p.low*cost.highlow + p.high*cost.lowhigh)
 p.high.new <- p.high * cost.lowhigh / (p.low*cost.highlow + p.high*cost.lowhigh)
 
+priors.orig <- c(p.high, p.low)
+
 priors <- c( p.high.new, p.low.new)  
 priors
 
@@ -23,8 +35,6 @@ priors
 # we know LDA,QDA, KNN, SVM
 # we need to verify gaussianity and same covariance between groups for LDA
 # Assumption of gaussianity
-
-result <- factor(data$result)
 
 high <- features[result==levels(result)[1],]
 low <- features[result==levels(result)[2],]
@@ -37,7 +47,6 @@ ps
 # we could however accept it at 1%.
 
 # since we are not so sure about gaussianity i would not use the barlett test.
-
 Shigh <- cov(high)
 Slow <- cov(low)
 Shigh/Slow
@@ -63,20 +72,31 @@ G <- 2
 misc <- table(class.true=result, class.assigned=predictions$class)
 APER <- 0
 for(g in 1:G)
- APER <- APER + sum(misc[g,-g])/sum(misc[g,]) * priors[g]
+ APER <- APER + sum(misc[g,-g])/sum(misc[g,]) * priors.orig[g]
 APER
 
 # estimating the AER with Leave one out cross validation
-errors_CV <- 0
-for(i in 1:60){
-  LdaCV.i <- lda(features[-i,], result[-i], prior=priors)
-  errors_CV <- errors_CV + as.numeric(predict(LdaCV.i,features[i,])$class != result[i])
-}
-errors_CV
+APER.cv <-NULL
+fulltable <- cbind(c(0,0),c(0,0))
 
-AERCV   <- sum(errors_CV)/length(result)
-AERCV
-# 30% of the time i will do an error
+for(i in 1:dim(features)[1]){
+  LdaCV.i <- lda(features[-i,], result[-i], prior=priors)
+  prediction <- predict(LdaCV.i, features[i,])
+  predicted <- ifelse(prediction$class == "high",1,2)
+  g.cv <- ifelse(result[i] == "high",1,2)
+  
+  if(predicted == g.cv)
+    fulltable[g.cv,g.cv]=fulltable[g.cv,g.cv]+1
+  if(predicted != g.cv)
+    fulltable[g.cv,-g.cv]=fulltable[g.cv,-g.cv]+1
+}
+
+AER.cv <- 0
+for(g in 1:G)
+  AER.cv <- AER.cv + sum(fulltable[g,-g])/sum(fulltable[g,]) * priors.orig[g]
+AER.cv
+
+# APER = AER.cv
 
 
 # to estimate the budget i will use the AERCV that is a better estimate of the actual error
@@ -84,9 +104,45 @@ N <- 1000
 Nhigh <- N * p.high
 Nlow <- N * p.low
 
-budget <- AERCV * Nhigh *cost.lowhigh + AERCV *Nlow*cost.highlow
+budget <- AERCV.lda * Nhigh *cost.lowhigh + AERCV.lda *Nlow*cost.highlow
 budget
 
 # with the old strategy
 budget.old <- 500*1000
 budget.old
+
+
+# trying QDA due to indecision in covariance structure
+fit.qda <- qda(features, result, prior=priors)
+
+predictions.qda <- predict(fit.qda,features)
+
+
+# APER
+G <- 2
+misc <- table(class.true=result, class.assigned=predictions.qda$class)
+APER.qda <- 0
+for(g in 1:G)
+  APER.qda <- APER.qda + sum(misc[g,-g])/sum(misc[g,]) * priors.orig[g]
+APER.qda
+
+# estimating the AER with Leave one out cross validation
+APER.cv.qda <-NULL
+fulltable.qda <- cbind(c(0,0),c(0,0))
+
+for(i in 1:dim(features)[1]){
+  QdaCV.i <- qda(features[-i,], result[-i], prior=priors)
+  prediction.qda <- predict(QdaCV.i, features[i,])
+  predicted.qda <- ifelse(prediction.qda$class == "high",1,2)
+  g.cv.qda <- ifelse(result[i] == "high",1,2)
+  
+  if(predicted.qda == g.cv.qda)
+    fulltable.qda[g.cv.qda,g.cv.qda]=fulltable.qda[g.cv.qda,g.cv.qda]+1
+  if(predicted.qda != g.cv.qda)
+    fulltable.qda[g.cv.qda,-g.cv.qda]=fulltable.qda[g.cv.qda,-g.cv.qda]+1
+}
+
+AER.cv.qda <- 0
+for(g in 1:G)
+  AER.cv.qda <- AER.cv.qda + sum(fulltable.qda[g,-g])/sum(fulltable.qda[g,]) * priors.orig[g]
+AER.cv.qda
