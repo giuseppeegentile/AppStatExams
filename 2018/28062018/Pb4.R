c225222f238d4511765e04da4d45e776e532badb
Giuseppe <giuseppe2.gentile@mail.polimi.it>
2024-06-11 09:56:40 +0200

    corretti beta

--- 2018/28062018/Pb4.R
+++ 2018/28062018/Pb4.R
@@ -17,8 +17,8 @@ options(rgl.printRglwidget = TRUE)
 
 data <- read.table("Hotels.txt",header=T)
 head(data)
-
-
+levels(factor(data$Season))
+levels(factor(data$Position))
 plot(data, pch=19)
 
 # price shows periodic pattern with t
@@ -41,14 +41,16 @@ y   <- data$price
 ## Parameter estimation -----------------------------------------------------------------------
 # Assumptions: E(Eps) = 0  and  Var(Eps) = sigma^2 
 
-fm <- lm(y ~ Position + Season  + Position:I(1+cos(4*pi*t/365))+ Season:I(1+cos(4*pi*t/365)),data=data)
+fm <- lm(y ~ Position + Season +I(1+cos(4*pi*t/365))+ Position:I(1+cos(4*pi*t/365))+ Season:I(1+cos(4*pi*t/365)),data=data)
 summary(fm) 
 # beta0.0 = 75.489
 # beta0.1 = 75.489 + 36.032
 # beta0.2 = 75.489 -32.468
+# beta0.3 = 75.489 + 36.032 -32.468
 # beta1.0 = 74.452
-# beta1.1 = 66.293
-# beta1.2 = -5.204
+# beta1.1 = 74.452 - 8.159
+# beta1.2 = 74.452 - 5.204
+# beta1.2 = 74.452 - 8.159 - 5.204
 
 # Residual standard error: (estimated variance of residual )^/1/2 estimate of sigma
 sqrt(sum(residuals(fm)^2)/fm$df)  
@@ -72,18 +74,33 @@ linearHypothesis(fm, rbind(c(0,0,1,0,0,0),
 # Seasonality is influential
 
 linearHypothesis(fm, rbind(c(0,1,0,0,0,0), 
-                           c(0,0,0,1,0,0),
-                           c(0,0,0,0,1,0)), c(0,0,0))
+                           c(0,0,0,0,1,0)), c(0,0))
 # Position is influential
 
-linearHypothesis(fm, rbind(c(0,0,0,0,0,1), 
-                           c(0,0,0,1,0,0),
-                           c(0,0,0,0,1,0)), c(0,0,0))
-# yes, as well
+linearHypothesis(fm, rbind(c(0,0,0,0,1,-1)), c(0))
+# not  influencing
 
+# Experiment
+#no significant difference in the temporal dynamic of the mean rates along the year depending on
+# the seasonality or on the position of the hotel beta4=beta5
+# y  = beta0 + beta1*Position + beta2*Season + beta3*I(1+cos(4*pi*t/365))+
+#   beta4*Position:I(1+cos(4*pi*t/365))+ beta5*Season:I(1+cos(4*pi*t/365))
+# y  = beta0 + beta1*Position + beta2*Season + beta3*I(1+cos(4*pi*t/365))+
+#   beta4*(Position+ Season):I(1+cos(4*pi*t/365))
+# 
+# # we need to create dummy variable to indicate both the season and position to 1
+# pos <- ifelse(data$Position == "Seafront", 1, 0)
+# season <- ifelse (data$Season == "WetSeason", 1, 0)
+# data$dummy <- rep(0, dim(data)[1])
+# for(i in 1:dim(data)[1])
+# {
+#   if(pos[i] == 1 && season[i] == 1) data$dummy[i] = 1
+# }
+# data$dummy <- as.factor(data$dummy)  
+# 
+# fm <- lm(y ~ Position + Season + dummy:I(1+cos(4*pi*t/365)),data=data)
+# summary(fm)
 
-fm <- lm(y ~ Position + Season  + Position:I(1+cos(4*pi*t/365)),data=data)
-summary(fm) 
 
 # max when cos (..) = 1 -> ... = 0 -> t = 0
 # coeff of wetseason is negative, so we don't put the wet season to get the max
